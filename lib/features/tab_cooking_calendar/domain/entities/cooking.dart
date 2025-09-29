e69a910542ea090a0309e6ccbe43402f9d53d948
yassan16 <ys.tech.github@gmail.com>
2025-09-29 14:06:34 +0900

    feat: 料理エンティティを仮実装

---
+++ lib/features/tab_cooking_calendar/domain/entities/cooking.dart
@@ -0,0 +1,34 @@
+/// 作った料理のエンティティ
+class Cooking {
+  final String id;
+  final String? userId;
+  // 料理名
+  final String name;
+  // 料理の種類
+  final String category;
+  // 食材id
+  final List<String>? ingredientIds;
+  // 調理手法id
+  final List<String>? cookingMethodIds;
+  // 料理の写真URL
+  final List<String>? imageUrlList;
+  // 詳細
+  final String? description;
+  // コメント
+  final String? comment;
+  // 作った日付
+  final DateTime madeDateTime;
+
+  Cooking({
+    required this.id,
+    this.userId,
+    required this.name,
+    required this.category,
+    this.ingredientIds,
+    this.cookingMethodIds,
+    this.imageUrlList,
+    this.description,
+    this.comment,
+    required this.madeDateTime,
+  });
+}
